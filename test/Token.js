37c0f2763b196c72452ef61642fd088d428ba425
Ondrej Juda <ondrej.juda@networg.com>
2022-11-17 06:37:51 +0100

    testing token deployment

--- test/Token.js
+++ test/Token.js
@@ -4,19 +4,38 @@ const { expect } = require('chai');
 
 describe('Token', () => {
   let token;
+  const name = 'Dapp University';
+  const symbol = 'DAPP';
+  const decimals = 18;
+  const totalSupply = 1_000_000;
 
-  it('deploys', async () => {
-    // fetch token from blockchain
-    const Token = await ethers.getContractFactory('Token');
-    token = await Token.deploy();
-    await token.deployed();
-  });
+  // beforeEach(async () => {
+  //   const Token = await ethers.getContractFactory('Token');
+  //   token = await Token.deploy();
+  // });
 
-  it('has correct name', async () => {
-    // read token name
-    const name = await token.name();
+  describe('Deployment', () => {
+    it('deployes', async () => {
+      const Token = await ethers.getContractFactory('Token');
+      token = await Token.deploy(name, symbol, totalSupply);
+      await token.deployed();
+    });
 
-    // check name correctnes
-    expect(name).to.equal('Dapp University');
+    it('has correct name', async () => {
+      expect(await token.name()).to.equal(name);
+    });
+
+    it('has correct symbol', async () => {
+      expect(await token.symbol()).to.equal(symbol);
+    });
+
+    it('has correct decimals', async () => {
+      expect(await token.decimals()).to.equal(decimals);
+    });
+
+    it('has correct total supply', async () => {
+      const value = ethers.utils.parseEther(totalSupply.toString());
+      expect(await token.totalSupply()).to.equal(value);
+    });
   });
 });
