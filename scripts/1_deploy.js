917e4db9f149d59c460520add35d95402866c27c
Ondrej Juda <ondrej.juda@networg.com>
2022-11-28 07:15:44 +0100

    deploy and seed exchange

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,14 +1,29 @@
-const { ethers } = require("hardhat");
-const hre = require("hardhat");
+const { ethers } = require('hardhat');
 
 async function main() {
   // fetch contract
-  const Token = await ethers.getContractFactory("Token");
+  const Token = await ethers.getContractFactory('Token');
+  const Exchange = await ethers.getContractFactory('Exchange');
+
+  // fetch accounts
+  const [deployer, feeAccount] = await ethers.getSigners();
 
   // deploy contract
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log(`Token Deployed to: ${token.address}`);
+  const dapp = await Token.deploy('Dapp University', 'DAPP', '1000000');
+  await dapp.deployed();
+  console.log(`DAPP Token Deployed to: ${dapp.address}`);
+
+  const mweth = await Token.deploy('Mock Wrapped Ether', 'mWETH', '1000000');
+  await mweth.deployed();
+  console.log(`mWETH Token Deployed to: ${mweth.address}`);
+
+  const mdai = await Token.deploy('Mock Dai', 'mDAI', '1000000');
+  await mdai.deployed();
+  console.log(`mDAI Token Deployed to: ${mdai.address}`);
+
+  const exchange = await Exchange.deploy(feeAccount.address, 10);
+  await exchange.deployed();
+  console.log(`Exchange Deployed to: ${exchange.address}`);
 }
 
 main()
